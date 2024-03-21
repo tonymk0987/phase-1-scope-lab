a7999672f4cfe9be622ec7b91e6aa1f30af9ac34
tonymk0987 <k.tony.muriuki@gmail.com>
2024-03-21 22:54:55 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
+}
+function setBestCustomer(){
+    bestCustomer = `not bob`
+}
+function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`
+}
+const leastFavoriteCustomer = `Tony`
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = `Mark`
+}
